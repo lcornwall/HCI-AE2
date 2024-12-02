d8a033d9676bbc687b5c1f600c38916ac2a43db6
“Libby <lcornwall7@gmail.com>
2024-12-02 19:53:54 +0000

    Adding counting to ratings functionality

--- pages/lecture/Lecture.js
+++ pages/lecture/Lecture.js
@@ -1,7 +1,23 @@
+import { useState } from 'react';
 import NavBar from '../../components/NavBar';
 import styles from './Lecture.module.css';
 
 export default function Lecture() {
+    const [ratings, setRatings] = useState({
+        necessary: 24,
+        worthwhile: 0,
+        optional: 2,
+    });
+
+
+    const handleRating = (ratingType) => {
+        setRatings((prevRatings) => ({
+            ...prevRatings,
+            [ratingType]: prevRatings[ratingType] + 1
+        }));
+
+    };
+
     return (
         <div className={styles.container}>
             <h1 className={styles.h1}>Database Systems - Lecture 1</h1>
@@ -16,16 +32,38 @@ export default function Lecture() {
                 </div>
                 <div className={styles['rating-container']}>
                     <p className={styles['student-count']}>
-                        <strong>24 </strong> students marked this as <strong>essential</strong>
+                        <strong>{ratings.necessary} </strong> students marked this as <strong>necessary</strong>.
+                    </p>
+                    <p className={styles['student-count']}>
+                        <strong>{ratings.worthwhile} </strong> students marked this as <strong>worthwhile</strong>.
+                    </p>
+                    <p className={styles['student-count']}>
+                        <strong>{ratings.optional} </strong> students marked this as <strong>optional</strong>.
                     </p>
                     <p>Rate the <strong>usefulness</strong> of this lecture:</p>
                     <div className={styles['rating-buttons']}>
-                        <button className={styles['rating-button']}>Necessary</button>
-                        <button className={styles['rating-button']}>Worthwhile</button>
-                        <button className={styles['rating-button']}>Optional</button>
+                        <button
+                            className={styles['rating-button']}
+                            onClick={() => handleRating('necessary')}
+                        >
+                            Necessary
+                        </button>
+                        <button
+                            className={styles['rating-button']}
+                            onClick={() => handleRating('worthwhile')}
+                        >
+                            Worthwhile
+                        </button>
+                        <button
+                            className={styles['rating-button']}
+                            onClick={() => handleRating('optional')}
+                        >
+                            Optional
+                        </button>
                     </div>
                 </div>
             </div>
+
             <NavBar />
         </div>
     );
